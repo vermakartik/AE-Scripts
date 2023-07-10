458982c1bba2faad8b2a758a3f072736097ae911
Kartik Verma <30172814+vermakartik@users.noreply.github.com>
2023-07-10 20:31:34 +0530

    Create ease_slider.js

---
+++ ease_slider.js
@@ -0,0 +1,8 @@
+// https://www.youtube.com/watch?v=EY5vTpS3aLc&list=PLW-zSkCnZ-gD8OcjTPu-u_Rxl9-kI9Xqr&index=42
+
+// no animation
+ease(value, 0, 0, 0, 0)
+
+// median animation
+// here 40 corresponds to 0, 100 to 100 
+ease(value, 40, 100, 0, 100)
